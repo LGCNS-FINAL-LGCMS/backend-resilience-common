7a0effd163c86ffc302bd918161c86336fa445ce
Sunhokim2 <mcwright@naver.com>
2025-08-27 15:39:33 +0900

    first commit

---
+++ src/main/java/com/lgcms/resiliencecommon/ResilienceCommonApplication.java
@@ -0,0 +1,13 @@
+package com.lgcms.resiliencecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilienceCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResilienceCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcms/resiliencecommon/ResilienceCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcms.resiliencecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilienceCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
